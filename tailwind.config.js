22d696d03792d28150d1cef21433b9f7b464e727
Антон Ханабеев <p8@iqvector.ru>
2021-03-25 07:47:24 +0300

    finish study

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 module.exports = {
-  purge: [
-    './src/**/*.html',
-    './src/**/*.js',
-  ],
+  purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary: '#FF6363',
+        secondary:{
+          100: '#E2E2D5',
+          200: '#888883',
+        }
+      },
+      fontFamily: {
+        body: ['Nunito']
+      }
+    },
   },
   variants: {
     extend: {},
